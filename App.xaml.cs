f5c33751eead6185fd60a0009f96abc6714e3144
Pawel062 <59065602+Pawel062@users.noreply.github.com>
2020-02-12 10:08:40 -0500

    Add files via upload

--- App.xaml.cs
+++ App.xaml.cs
@@ -8,9 +8,20 @@ using System.Windows;
 
 namespace WpfApp1
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
+    //Author : Michael Figueroa
+    //Name: AdHoc.xaml.cs
+    //Function Name:
+    //Purpose: <summary>
+    /// Interaction logic for App.xaml. Declares this as an application and defines what will be used.
     /// </summary>
+    //Parameters:
+    //Return Value: 
+    //Local Variables: 
+    //Algorithm: 
+    //Version: 
+    //Date modified: 
+    //Assistance Received: 
+
     public partial class App : Application
     {
     }
