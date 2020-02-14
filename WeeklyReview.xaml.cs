ce5e197cf310e7be345c9f62225e0bf20689261d
mikey2695 <52287707+mikey2695@users.noreply.github.com>
2020-02-14 15:57:55 -0500

    Mike Fig Comments
    Team,
    These are the forms on which I have made comments so far. I would like you to skim the comments and see if you can get a good grasp of what is happening behind the scenes in the database. Let me know if you have any questions.

--- WeeklyReview.xaml.cs
+++ WeeklyReview.xaml.cs
@@ -27,8 +27,18 @@ namespace WpfApp2
         public string connectionString = ConfigurationManager.ConnectionStrings["conString"].ConnectionString;//ConnectionString comes from App.config
         private string[] arr;                       //local variable to store login-based user data
         private DataRowView reportRow;       //local variable to store the row of data in the from a specific row in the Report DataGrid
-        private bool fullHistoryChosen = false;
+        private bool fullHistoryChosen = false; //bool that determines whether the user has toggled on full status history for a particular issue
 
+        /*Name: Michael Figueroa
+       Function Name: WeeklyReview
+       Purpose: Constructor for the WeeklyReview form
+       Parameters: string[] user_data
+       Return Value: None
+       Local Variables: None
+       Algorithm: Calls FillStatusComboBox, then FillSystemComboBox, both indexes set to "All Open", then SetStatusComboVis and BindDataGrid are called
+       Date modified: Prior to 1/1/20
+       Assistance Received: N/A
+       */
         public WeeklyReview(string[] user_data)
         {
             InitializeComponent();
@@ -41,12 +51,32 @@ namespace WpfApp2
             BindDataGrid();
         }
 
+        /*Name: Michael Figueroa
+       Function Name: BindDataGrid
+       Purpose: Binds data to datagrid
+       Parameters: None
+       Return Value: None
+       Local Variables: report
+       Algorithm: Calls FillWeeklyReview
+       Date modified: Prior to 1/1/20
+       Assistance Received: N/A
+       */
         private void BindDataGrid()
         {
             DataTable report = new DataTable();
             FillWeeklyReview(report);
         }
 
+        /*Name: Michael Figueroa
+        Function Name: SetStatusComboVis
+        Purpose: Sets visibility of StatusComboBox and StatusText (StatusComboBox label)
+        Parameters: None
+        Return Value: None
+        Local Variables: None
+        Algorithm: If All Open from SystemComboBox is chosen, then statuscombobox not visible; else, it is.
+        Date modified: Prior to 1/1/20
+        Assistance Received: N/A
+        */
         private void SetStatusComboVis()
         {
             if(SystemComboBox.SelectedItem.ToString() == "All Open")
@@ -61,6 +91,17 @@ namespace WpfApp2
             }
         }
 
+        /*Name: Michael Figueroa
+        Function Name: FillSystemComboBox
+        Purpose: Sets visibility of StatusComboBox and StatusText (StatusComboBox label)
+        Parameters: None
+        Return Value: None
+        Local Variables: query
+        Algorithm: Calls SystemsInReportQuery to assign value to query. If status chosen from ComboBox is All Open, then All Open is added to SystemComboBox. Then, all systems that have an open issue for the week are
+        added to the SystemComboBox using query. 
+        Date modified: Prior to 1/1/20
+        Assistance Received: N/A
+        */
         private void FillSystemComboBox()
         {
             string query = SystemsInReportQuery();
@@ -83,6 +124,16 @@ namespace WpfApp2
             }
         }
 
+        /*Name: Michael Figueroa
+       Function Name: WeeklyQuery
+       Purpose: Sets query used for WeeklyReview
+       Parameters: None
+       Return Value: string
+       Local Variables: weeklyQuery
+       Algorithm: query is set based on what options are chosen in System and Status comboboxes
+       Date modified: Prior to 1/1/20
+       Assistance Received: N/A
+       */
         private string WeeklyQuery()
         {
             string weeklyQuery;
@@ -115,12 +166,32 @@ namespace WpfApp2
             return weeklyQuery;
         }
 
+        /*Name: Michael Figueroa
+        Function Name: FillStatusComboBox
+        Purpose: Fills Status combobox
+        Parameters: None
+        Return Value: None
+        Local Variables: None
+        Algorithm: None 
+        Date modified: Prior to 1/1/20 - May no longer be needed, Mike may want to get rid of it idk 
+        Assistance Received: N/A
+        */
         private void FillStatusComboBox()
         {
             StatusComboBox.Items.Add("Open");
             StatusComboBox.Items.Add("Closed");
         }
 
+        /*Name: Michael Figueroa
+        Function Name: FillWeeklyReview
+        Purpose: Fills Weekly Review table
+        Parameters: DataTable table
+        Return Value: None
+        Local Variables: None
+        Algorithm: WeeklyQuery() is executed, then table is filled, binded to DataGrid
+        Date modified: Prior to 1/1/20 
+        Assistance Received: N/A
+        */
         private void FillWeeklyReview(DataTable table)
         {
             using (SqlConnection con = new SqlConnection(connectionString))
@@ -152,6 +223,16 @@ namespace WpfApp2
                 }
         }
 
+        /*Name: Michael Figueroa
+        Function Name: MarkAsReviewed
+        Purpose: Allows manager/supervisor to clear items off of weekly review form after meeting is held
+        Parameters: None
+        Return Value: None
+        Local Variables: None
+        Algorithm: Calls WeeklyQuery() to execute command and then uses reader to go through each record and update WeeklyReview column to 0 in order to clear it off the form
+        Date modified: Prior to 1/1/20 
+        Assistance Received: N/A
+        */
         private void MarkAsReviewed()
         {
             using (SqlConnection con = new SqlConnection(connectionString))
@@ -192,6 +273,17 @@ namespace WpfApp2
                 }
         }
 
+        /*Name: Michael Figueroa
+         Function Name: EditButton_Click
+         Purpose: Event handler for edit button click
+         Parameters: Auto-generated
+         Return Value: None
+         Local Variables: DataRowView priorBySystemRow
+         Algorithm: The DataRow in which the Edit button was clicked is retrieved, and the EditRecord form is opened using that DataRowView in the constructor
+         Version: 2.0.0.4
+         Date modified: Prior to 1/1/20 - This method will be simplified by Mike at a later date
+         Assistance Received: N/A
+         */
         private void EditRecord_Click(object sender, RoutedEventArgs e)
         {
             try
@@ -212,6 +304,17 @@ namespace WpfApp2
             }
         }
 
+        /*Name: Michael Figueroa
+         Function Name: Status_SelectionChanged
+         Purpose: Event handler for StatusComboBox
+         Parameters: Auto-generated
+         Return Value: None
+         Local Variables: None
+         Algorithm: SystemComboBox is cleared, then re-filled using FillSystemComboBox(), SelectedIndex is set to 0, then SetStatusComboVis and BindDataGrid are called
+         Version: 2.0.0.4
+         Date modified: Prior to 1/1/20  
+         Assistance Received: N/A
+         */
         private void Status_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             SystemComboBox.Items.Clear();
@@ -221,6 +324,17 @@ namespace WpfApp2
             BindDataGrid();
         }
 
+        /*Name: Michael Figueroa
+         Function Name: GetComboBoxValue
+         Purpose: retrieves string value of current status chosen from comboBox
+         Parameters: Auto-generated
+         Return Value: string
+         Local Variables: None
+         Algorithm: is selectedIndex is all open, then "Open" is return value; else, Closed is returned
+         Version: 2.0.0.4
+         Date modified: Prior to 1/1/20  
+         Assistance Received: N/A
+         */
         private string GetComboBoxValue()
         {
             if (StatusComboBox.SelectedIndex == 0)
@@ -234,6 +348,17 @@ namespace WpfApp2
             }
         }
 
+        /*Name: Michael Figueroa
+         Function Name: MarkReviewed_Click
+         Purpose: Event handler for Mark Reviewed click
+         Parameters: Auto-generated
+         Return Value: None
+         Local Variables: None
+         Algorithm: if MessageBox option chosen is yes, then MarkAsReviews is called along with BindDataGrid
+         Version: 2.0.0.4
+         Date modified: Prior to 1/1/20 
+         Assistance Received: N/A
+         */
         private void MarkReviewed_Click(object sender, RoutedEventArgs e)
         {
             MessageBoxResult result = MessageBox.Show("Mark All as Reviewed?", "Mark as Reviewed", MessageBoxButton.YesNo);
@@ -244,6 +369,17 @@ namespace WpfApp2
             }
         }
 
+        /*Name: Michael Figueroa
+         Function Name: SystemsInReportQuery
+         Purpose: Query that tells us what systems are included in this week's Weekly Review
+         Parameters: None
+         Return Value: None
+         Local Variables: None
+         Algorithm: If All Open is chosen, all non-closed items are chosen; else, if open, all non-closed items are chosen; else, all closed items are queried.
+         Version: 2.0.0.4
+         Date modified: Prior to 1/1/20 - the else if clause is not needed. 
+         Assistance Received: N/A
+         */
         private string SystemsInReportQuery()
         {
             string query;
@@ -273,7 +409,18 @@ namespace WpfApp2
 
             return query;
         }
-       
+
+        /*Name: Michael Figueroa
+         Function Name: BindHistoryGrid
+         Purpose: Query that pulls all statuses for a particular issue
+         Parameters: string TaskNum
+         Return Value: None
+         Local Variables: query
+         Algorithm: query is assigned a string value; then the data from the query is binded to the FullHistory datagrid
+         Version: 2.0.0.4
+         Date modified: Prior to 1/1/20 - the else if clause is not needed. 
+         Assistance Received: N/A
+         */
         private void BindHistoryGrid(string TaskNum)
         {
             using (SqlConnection connection = new SqlConnection(connectionString))
@@ -311,6 +458,17 @@ namespace WpfApp2
             //Only displays the full history table if the user clicks the Status History button
         }
 
+        /*Name: Michael Figueroa
+         Function Name: History_Click
+         Purpose: Event handler for history button
+         Parameters: Auto-Generated
+         Return Value: None
+         Local Variables: query
+         Algorithm: reportRow is assigned using the row that was clicked; then BindHistoryGrid is called; if fullHistoryChosen is true, the FullHistory datagrid is visible, else, it is not
+         Version: 2.0.0.4
+         Date modified: Prior to 1/1/20 - the else if clause is not needed. 
+         Assistance Received: N/A
+         */
         private void History_Click(object sender, RoutedEventArgs e)
         {
             reportRow = (DataRowView)((Button)e.Source).DataContext; //Retrieves the information for the row that the user clicks
@@ -330,6 +488,17 @@ namespace WpfApp2
             }
         }
 
+        /*Name: Michael Figueroa
+        Function Name: SystemComboBox_SelectionChanged
+        Purpose: Event handler for when SystemComboBox changes
+        Parameters: Auto-Generated
+        Return Value: None
+        Local Variables: query
+        Algorithm: if SystemComboBox is not null, SetStatusComboVis and BindDataGrid are called
+        Version: 2.0.0.4
+        Date modified: Prior to 1/1/20 - the else if clause is not needed. 
+        Assistance Received: N/A
+        */
         private void SystemComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (SystemComboBox.SelectedItem != null)
